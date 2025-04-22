74168f00776a4529f988485714442a0bdf07aa5f
marryyx <markfrancyzov@gmail.com>
2025-04-22 17:21:15 +0300

    last commit tabs-slider

--- src/js/components/tabs.component.js
+++ src/js/components/tabs.component.js
@@ -9,13 +9,23 @@ export class Tabs {
         const step = document.querySelectorAll('.tabs__steps-button');
         const contentTab = document.querySelectorAll('.tabs__content-card');
 
+        const container = document.querySelector('.tabs__content-inner');
+
         step.forEach((item, index) => {
             item.addEventListener('click', () => {
+                let currentIndex = -1;
+                const allCards = 4;
+
                 step.forEach(elm => elm.classList.remove('--active'));
                 item.classList.add('--active');
 
-                contentTab.forEach(elm => elm.classList.remove('--active'));
-                contentTab[index].classList.add('--active');
+                // if (index < 0 || index >= allCards) return
+
+                if (index > currentIndex) {
+                    container.style.setProperty('--tabs-contetn-card-transform-x', `-${index * 100}%`);
+                  } else if (index < currentIndex) {
+                    container.style.setProperty('--tabs-contetn-card-transform-x', `-${index * 100}%`);
+                  }
             });
         });
     }
