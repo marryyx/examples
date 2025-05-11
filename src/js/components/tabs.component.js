022fa7e97d99f64c89787f379e416471050b90ee
marryyx <markfrancyzov@gmail.com>
2025-05-11 14:09:23 +0300

    corrected js

--- src/js/components/tabs.component.js
+++ src/js/components/tabs.component.js
@@ -3,25 +3,41 @@ export class TabsComponent {
         this.host = document.querySelector('.tabs');
     }
 
-    setSliderPosition(val) {
-        const container = this.host.querySelector('.tabs__content-inner');
-        container.style.setProperty('--tabs-contetn-card-transform-x', `-${val * 100}%`);
-    }
+    // setSliderPosition(val) {
+    //     const container = this.host.querySelector('.tabs__content-inner');
+    //     container.style.setProperty('--tabs-contetn-card-transform-x', `-${val * 100}%`);
+    // }
 
-    activeSlide(activeElm) {
+    activeStep(activeElm) {
         this.step.forEach(elm => elm.classList.remove('--active'));
         activeElm.classList.add('--active');
     }
+
+    slideCard(datasetStep) {
+        this.card.forEach(item => item.classList.remove('--is-active'));
+
+        this.card.forEach(elm => {
+            const dataset = elm.dataset.card;
+
+            if (datasetStep == dataset) {
+                elm.classList.add('--is-active');
+            }
+        });
+    }
+
 
     init() {
         if (!this.host) return;
 
         this.step = this.host.querySelectorAll('.tabs__steps-button');
+        this.card = this.host.querySelectorAll('.slider-card');
 
-        this.step.forEach((item, index) => {
+        this.step.forEach(item => {
             item.addEventListener('click', () => {
-                this.activeSlide(item)
-                this.setSliderPosition(index);
+                this.activeStep(item)
+                this.slideCard(item.dataset.step);
+                // this.setSliderPosition(index);
+                // const dataset = item.dataset.step;
             });
         });
     }
