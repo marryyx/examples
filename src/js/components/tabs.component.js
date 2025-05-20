67cb19d59ca472f398350072ef0dad2c5094e558
marryyx <markfrancyzov@gmail.com>
2025-05-20 10:33:29 +0300

    complete task animate js

--- src/js/components/tabs.component.js
+++ src/js/components/tabs.component.js
@@ -1,3 +1,37 @@
+const SLIDER_ANIMATION_KEYFRAME = [
+    {
+        opacity: 0,
+        // transform: "translateX(100%)"
+    },
+    {
+        opacity: 1,
+        // transform: "translateX(0)"
+    },
+];
+
+const SLIDER_ANIMATION_KEYFRAME2 = [
+    { opacity: 0, transform: "translateX(100%)" },
+    { opacity: 1, transform: "translateX(0)" },
+];
+
+const SLIDER_ANIMATION_KEYFRAME3 = [
+    { opacity: 0, transform: "translateY(-100%)" },
+    { opacity: 1, transform: "translateY(0)" },
+];
+
+const SLIDER_ANIMATION_TIMING = {
+    fill: "both",
+    delay: 250,
+    duration: 850
+};
+
+const SLIDER_ANIMATION_TIMING2 = {
+    fill: "both",
+    delay: 0,
+    duration: 550,
+    // easing: "cubic-bezier(.69,-0.45,.82,1.58)"
+};
+
 export class TabsComponent {
     constructor() {
         this.host = document.querySelector('.tabs');
@@ -13,27 +47,31 @@ export class TabsComponent {
         activeElm.classList.add('--active');
     }
 
+    activeSlide(elm) {
+        const animationOption = elm.dataset.card;
+
+        switch (animationOption) {
+            case "get-paid":
+                elm.animate(SLIDER_ANIMATION_KEYFRAME, SLIDER_ANIMATION_TIMING);
+                break;
+            case "borrow":
+                elm.animate(SLIDER_ANIMATION_KEYFRAME2, SLIDER_ANIMATION_TIMING);
+                break;
+            case "invest":
+                elm.animate(SLIDER_ANIMATION_KEYFRAME3, SLIDER_ANIMATION_TIMING2);
+                break;
+            case "transact":
+                elm.animate(SLIDER_ANIMATION_KEYFRAME, SLIDER_ANIMATION_TIMING);
+                break;
+        };
+
+        elm.classList.add('--is-active');
+    }
+
     slideCard(datasetStep) {
         this.card.forEach(item => item.classList.remove('--is-active'));
 
-        const cardsAnim = [
-            { opacity: "0" },
-            { opacity: "1" },
-        ];
-
-        const cardsTiming = {
-            duration: 950,
-            iterations: 0.95,
-        };
-
-        this.card.forEach(elm => {
-            const dataset = elm.dataset.card;
-
-            if (datasetStep == dataset) {
-                elm.animate(cardsAnim, cardsTiming);
-                elm.classList.add('--is-active');
-            }
-        });
+        this.card.forEach(elm => (elm.dataset.card == datasetStep) && this.activeSlide(elm));
     }
 
 
