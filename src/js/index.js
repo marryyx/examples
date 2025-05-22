cd3655c565bdd80f56bfd1672b50b2cf3db4d967
marryyx <markfrancyzov@gmail.com>
2025-05-23 01:41:51 +0300

    preparation of the project sliders, 1 slider made

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,4 @@
-import { TabsComponent } from './components/tabs.component'
-
 document.addEventListener("DOMContentLoaded", () => {
-    const tabs = new TabsComponent();
-
-    tabs.init()
-  
     console.log('Page Loaded✅');
 });
   
